274e261bd34c19ab5c9e04764db63860f40dfabf
hassan <hchoudhury20@hotmail.com>
2024-02-07 16:28:53 +0000

    add db context and scaffold controllers

--- obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+299fb2d294a93036908924ee66d4d127c8ab0154")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fa8fe519fb054f2cf3b9fc24230a4b1fe186434")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
